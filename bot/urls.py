4d8f706d63440535f34217e9aeaab8fa4992a2cb
Kakkart16 <kakkartushar.16@gmail.com>
2023-11-06 20:41:47 +0530

    added register and login option

--- bot/urls.py
+++ bot/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.chatbot, name = 'chatbot' ),   
+    
+    path('', views.chatbot, name = 'chatbot' ),
+    path('login', views.login, name = 'login'),
+    path('register', views.register, name = 'register'),
+    path('logout', views.logout, name = 'logout'),   
+    
 ]
